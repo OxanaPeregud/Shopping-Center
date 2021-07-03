5573428f10000e9427f5ce6a59f3f1f071f29380
Oxana Peregud <76512695+OxanaPeregud@users.noreply.github.com>
2021-07-03 09:58:22 +0300

    add: full form validation

--- src/main/webapp/view/store-form.jsp
+++ src/main/webapp/view/store-form.jsp
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Application Form</title>
+    <title>Store Information Form</title>
     <link rel="stylesheet" href="${pageContext.request.contextPath}/style.css">
     <script src="view/validation.js"></script>
 </head>
@@ -11,7 +11,7 @@
 <body>
 <form name="storeForm" onsubmit="return validateForm()"
       action="${pageContext.request.contextPath}/insert-store" method="post">
-    <h2>Store information</h2>
+    <h2>Store Information</h2>
 
     <div class="row">
         <label>Store Name</label>
@@ -24,7 +24,7 @@
     <div class="row">
         <label>Store Description</label>
         <label>
-            <input type="text" name="description">
+            <input type="text" name="description" maxlength="255">
         </label>
         <div class="error" id="descriptionErr"></div>
     </div>
@@ -34,27 +34,31 @@
         <label>
             <input type="text" name="discount">
         </label>
+        <div class="error" id="discountErr"></div>
     </div>
 
     <div class="row">
         <label>Discount Start Date</label>
         <label>
-            <input type="date" name="discountStartDate">
+            <input type="date" name="discountStartDate" required pattern="\d{4}-\d{2}-\d{2}">
+            <span class="validity"></span>
         </label>
     </div>
 
     <div class="row">
         <label>Discount End Date</label>
         <label>
-            <input type="date" name="discountEndDate">
+            <input type="date" name="discountEndDate" required pattern="\d{4}-\d{2}-\d{2}">
+            <span class="validity"></span>
         </label>
     </div>
 
     <div class="row">
         <label>Location</label>
         <label>
-            <input type="text" name="location">
+            <input type="text" name="location" maxlength="255">
         </label>
+        <div class="error" id="locationErr"></div>
     </div>
 
     <div class="row">
--- src/main/webapp/view/validation.js
+++ src/main/webapp/view/validation.js
@@ -5,14 +5,19 @@ function printError(elemId, hintMsg) {
 function validateForm() {
     var name = document.storeForm.name.value;
     var description = document.storeForm.description.value;
+    var discount = document.storeForm.discount.value;
+    var discountStartDate = document.storeForm.discountStartDate.value;
+    var discountEndDate = document.storeForm.discountEndDate.value;
+    var location = document.storeForm.location.value;
     var email = document.storeForm.email.value;
     var mobile = document.storeForm.mobile.value;
 
-    var nameErr = emailErr = mobileErr = true;
+    var nameErr = descriptionErr = discountErr = locationErr = emailErr = mobileErr = true;
+
     if (name === "") {
         printError("nameErr", "Please enter Store name");
     } else {
-        var regex1 = /^[a-zA-Z\s]+$/;
+        var regex1 = /^[a-zA-Z0-9\W\s]+$/;
         if (regex1.test(name) === false) {
             printError("nameErr", "Please enter a valid Store name");
         } else {
@@ -24,7 +29,7 @@ function validateForm() {
     if (description === "") {
         printError("descriptionErr", "Please enter Store description");
     } else {
-        var regex2 = /^[a-zA-Z\s]+$/;
+        var regex2 = /^[a-zA-Z0-9\W\s]+$/;
         if (regex2.test(description) === false) {
             printError("descriptionErr", "Please enter a valid Store description");
         } else {
@@ -33,11 +38,35 @@ function validateForm() {
         }
     }
 
-    if (email === "") {
-        printError("emailErr", "Please enter your email address");
+    if (discount === "") {
+        printError("discountErr", "Please enter discount");
     } else {
-        var regex3 = /^\S+@\S+\.\S+$/;
-        if (regex3.test(email) === false) {
+        var regex3 = /^[0-9]+$/;
+        if (regex3.test(discount) === false) {
+            printError("discountErr", "Please enter a valid discount (only numeric characters allowed)");
+        } else {
+            printError("discountErr", "");
+            discountErr = false;
+        }
+    }
+
+    if (location === "") {
+        printError("locationErr", "Please enter Store location");
+    } else {
+        var regex4 = /^[a-zA-Z0-9\W\s]+$/;
+        if (regex4.test(location) === false) {
+            printError("locationErr", "Please enter a valid Store location");
+        } else {
+            printError("locationErr", "");
+            locationErr = false;
+        }
+    }
+
+    if (email === "") {
+        printError("emailErr", "Please enter Store email address");
+    } else {
+        var regex5 = /^\S+@\S+\.\S+$/;
+        if (regex5.test(email) === false) {
             printError("emailErr", "Please enter a valid email address");
         } else {
             printError("emailErr", "");
@@ -46,10 +75,10 @@ function validateForm() {
     }
 
     if (mobile === "") {
-        printError("mobileErr", "Please enter your mobile number");
+        printError("mobileErr", "Please enter Store mobile number");
     } else {
-        var regex4 = /^[1-9]\d{11}$/;
-        if (regex4.test(mobile) === false) {
+        var regex6 = /^[1-9]\d{11}$/;
+        if (regex6.test(mobile) === false) {
             printError("mobileErr", "Please enter a valid 12 digit mobile number");
         } else {
             printError("mobileErr", "");
@@ -57,12 +86,16 @@ function validateForm() {
         }
     }
 
-    if ((nameErr || emailErr || mobileErr) === true) {
+    if ((nameErr || descriptionErr || discountErr || locationErr || emailErr || mobileErr) === true) {
         return false;
     } else {
         var dataPreview = "You've entered the following details: \n" +
             "Store Name: " + name + "\n" +
             "Store Description: " + description + "\n" +
+            "Discount: " + discount + "\n" +
+            "Discount Start Date: " + discountStartDate + "\n" +
+            "Discount End Date: " + discountEndDate + "\n" +
+            "Store Location: " + location + "\n" +
             "Email Address: " + email + "\n" +
             "Mobile Number: " + mobile + "\n";
         alert(dataPreview);
